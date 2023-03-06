268217e39031c047d3b8cd56273313f42ca26f79
AntoineBellet <spynetantoine@gmail.com>
2023-03-06 14:09:54 +0100

    build: vérification des champs rentrer par l'utilisateur, conditions selon certaines situations salariales.

--- Program.cs
+++ Program.cs
@@ -7,28 +7,59 @@ class Program
     {
         Console.WriteLine("Enter your salary before tax ");
 
-        int salary = int.Parse(Console.ReadLine());
+        bool salaryIsInt = int.TryParse(Console.ReadLine(), out int salary);
 
         Console.WriteLine("Enter the value of tax ");
 
-        double taxe = int.Parse(Console.ReadLine());
+        bool taxIsInt = double.TryParse(Console.ReadLine(), out double tax);
 
-        int salaryNet = Impots(salary,taxe);
+        if (salaryIsInt){
+            if (taxIsInt)
+            {
+                int salaryNet = Impots(salary,tax);
+                Console.WriteLine("Salaire Net:  " + salaryNet);
+                Console.ReadLine();
 
-        Console.WriteLine("Salaire Net:  " + salaryNet);
-        Console.ReadLine();
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Your salary or tax are'nt integer");
+        }
+
+
 
     }
     static int Impots(int a, double b)
     {
         int salary = a;
-
+        
         double taxes = b / 100;
 
         int salaryTemp= (int)(a * taxes);
 
-        salary = salary - salaryTemp;
 
+        if (salary > 50000)
+        {
+            Console.WriteLine("You should to give many money to downgarde your tax !");
+            salary = salary - salaryTemp;
+            return salary;
+        }
+        if (salary / 12 > 1500)
+        {
+            Console.WriteLine("No probleme, it's OK for a work-study !");
+            salary = salary - salaryTemp;
+            return salary;
+        }
+        if (salary >= 40000 & salary <= 30000){
+            Console.WriteLine("You should to do a bac+5 at Cesi ;)");
+            salary = salary - salaryTemp;
+            return salary;
+        }
+
+
+        salary = salary - salaryTemp;
         return salary;
 
     }
