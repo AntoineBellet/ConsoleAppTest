704d30e595b18d8d3c4ff8c0cc4424f60cc9ddc3
AntoineBellet <spynetantoine@gmail.com>
2023-03-06 11:15:43 +0100

    build: affichage données fixe

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 10;
+        string name = "Rollon Normand";
+        double pourcentage = 10.23;
+        char gender = 'M';
+        bool isVerified = true;
+        Console.WriteLine("Id:  " + number);
+        Console.WriteLine("Name:  " + name);
+        Console.WriteLine("Pourcentage:  " + pourcentage); Console.WriteLine("Gender:  " + gender);
+        Console.WriteLine("Verified:  " + isVerified);
+        Console.ReadLine();
+
+    }
+}
