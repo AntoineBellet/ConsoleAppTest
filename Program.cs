2ee3ce59601998c88cfd8c44c4d50b2e1793ffb1
AntoineBellet <spynetantoine@gmail.com>
2023-03-06 16:12:21 +0100

    build: utilisation de tableau pour afficher le salair par moins, prime de fin d'année et mois de vacances.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ class Program
 
     static void Main(string[] args)
     {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
         Console.WriteLine("Enter your salary before tax ");
 
         bool salaryIsInt = int.TryParse(Console.ReadLine(), out int salary);
@@ -17,9 +19,9 @@ class Program
             if (taxIsInt)
             {
                 int salaryNet = Impots(salary,tax);
-                Console.WriteLine("Salaire Net:  " + salaryNet);
+                Console.WriteLine("Salaire Net annuel:  " + salaryNet);
+                SalaryPerMonth(salaryNet);
                 Console.ReadLine();
-
             }
 
         }
@@ -39,28 +41,51 @@ class Program
 
         int salaryTemp= (int)(a * taxes);
 
-
-        if (salary > 50000)
+        switch (salary)
         {
-            Console.WriteLine("You should to give many money to downgarde your tax !");
-            salary = salary - salaryTemp;
-            return salary;
+            case >= 50000:
+                Console.WriteLine("You should to give many money to downgarde your tax !");
+                salary = salary - salaryTemp;
+                break;
+            case  < 1500 * 12:
+                Console.WriteLine("No probleme, it's OK for a work-study !");
+                salary = salary - salaryTemp;
+                break;
+            case int x when x <= 40000 && x >= 30000:
+                Console.WriteLine("You should to do a bac+5 at Cesi ;)");
+                salary = salary - salaryTemp;
+                break;
         }
-        if (salary / 12 > 1500)
-        {
-            Console.WriteLine("No probleme, it's OK for a work-study !");
-            salary = salary - salaryTemp;
-            return salary;
-        }
-        if (salary >= 40000 & salary <= 30000){
-            Console.WriteLine("You should to do a bac+5 at Cesi ;)");
-            salary = salary - salaryTemp;
-            return salary;
-        }
-
 
         salary = salary - salaryTemp;
         return salary;
+    }
+
+    static void SalaryPerMonth(int salary) 
+    {
+        int salaryNetPerMonth = salary / 12;
+
+        int prime = (int)(salary * 0.1);
+
+        string[] monthCalendar = new string[] { "Janaruy", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December", "Prime"};
+
+
+        for (int i = 0; i < monthCalendar.Length; i++)
+            {
+                if (monthCalendar[i] == "August")
+                {
+                    Console.WriteLine(monthCalendar[i] + " : Vacances");
+                }
+                else if (monthCalendar[i] == "Prime")
+                {
+                    Console.WriteLine(monthCalendar[i] + " de fin d'année : " + prime + "€");
+                }
+                else
+                {
+                    Console.WriteLine(monthCalendar[i] + " : " + salaryNetPerMonth + "€");
+                }
+            }
+        }
 
     }
-}
