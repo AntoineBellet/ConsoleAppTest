0c1c9d3457de34f8a6233ec3c42a6e3aee7a1913
AntoineBellet <spynetantoine@gmail.com>
2023-03-06 12:00:00 +0100

    build: calcul du salaire après taxe avec demande à l'utilisateur du slaire brut et de la taxe

--- Program.cs
+++ Program.cs
@@ -1,17 +1,35 @@
-﻿class Program
+﻿using System.Runtime.ConstrainedExecution;
+
+class Program
 {
+
     static void Main(string[] args)
     {
-        int number = 10;
-        string name = "Rollon Normand";
-        double pourcentage = 10.23;
-        char gender = 'M';
-        bool isVerified = true;
-        Console.WriteLine("Id:  " + number);
-        Console.WriteLine("Name:  " + name);
-        Console.WriteLine("Pourcentage:  " + pourcentage); Console.WriteLine("Gender:  " + gender);
-        Console.WriteLine("Verified:  " + isVerified);
+        Console.WriteLine("Enter your salary before tax ");
+
+        int salary = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the value of tax ");
+
+        double taxe = int.Parse(Console.ReadLine());
+
+        int salaryNet = Impots(salary,taxe);
+
+        Console.WriteLine("Salaire Net:  " + salaryNet);
         Console.ReadLine();
+
+    }
+    static int Impots(int a, double b)
+    {
+        int salary = a;
+
+        double taxes = b / 100;
+
+        int salaryTemp= (int)(a * taxes);
+
+        salary = salary - salaryTemp;
+
+        return salary;
 
     }
 }
