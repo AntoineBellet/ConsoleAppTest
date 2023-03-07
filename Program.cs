a08cd7772a2e9c4fec7bd75e7ce3422e47e9d6a2
AntoineBellet <spynetantoine@gmail.com>
2023-03-07 10:09:25 +0100

    build: try catch avec le pourcentage de la prime de fin d'année et salaires par mois

--- Program.cs
+++ Program.cs
@@ -15,10 +15,11 @@ class Program
 
         bool taxIsInt = double.TryParse(Console.ReadLine(), out double tax);
 
-        if (salaryIsInt){
+        if (salaryIsInt)
+        {
             if (taxIsInt)
             {
-                int salaryNet = Impots(salary,tax);
+                int salaryNet = Impots(salary, tax);
                 Console.WriteLine("Salaire Net annuel:  " + salaryNet);
                 SalaryPerMonth(salaryNet);
                 Console.ReadLine();
@@ -36,10 +37,10 @@ class Program
     static int Impots(int a, double b)
     {
         int salary = a;
-        
+
         double taxes = b / 100;
 
-        int salaryTemp= (int)(a * taxes);
+        int salaryTemp = (int)(a * taxes);
 
         switch (salary)
         {
@@ -47,7 +48,7 @@ class Program
                 Console.WriteLine("You should to give many money to downgarde your tax !");
                 salary = salary - salaryTemp;
                 break;
-            case  < 1500 * 12:
+            case < 1500 * 12:
                 Console.WriteLine("No probleme, it's OK for a work-study !");
                 salary = salary - salaryTemp;
                 break;
@@ -61,31 +62,47 @@ class Program
         return salary;
     }
 
-    static void SalaryPerMonth(int salary) 
+    static void SalaryPerMonth(int salary)
     {
-        int salaryNetPerMonth = salary / 12;
+        double salaryNetPerMonth = salary / 12;
 
-        int prime = (int)(salary * 0.1);
-
-        string[] monthCalendar = new string[] { "Janaruy", "February", "March", "April", "May", "June",
+        string[] monthsCalendar = new string[] { "Janaruy", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December", "Prime"};
 
-
-        for (int i = 0; i < monthCalendar.Length; i++)
+        try
+        {
+            Console.WriteLine("Entrer le pourcentage de prime que vous souhaiter");
+            int pourcentagePrime = int.Parse(Console.ReadLine());
+            double prime = salary * pourcentagePrime / 100;
+            for (int i = 0; i < monthsCalendar.Length; i++)
             {
-                if (monthCalendar[i] == "August")
+                if (monthsCalendar[i] == "August")
                 {
-                    Console.WriteLine(monthCalendar[i] + " : Vacances");
+                    Console.WriteLine(monthsCalendar[i] + " : Vacances");
+                    continue;
                 }
-                else if (monthCalendar[i] == "Prime")
+                else if (monthsCalendar[i] == "Prime")
                 {
-                    Console.WriteLine(monthCalendar[i] + " de fin d'année : " + prime + "€");
+                    Console.WriteLine(monthsCalendar[i] + " de fin d'année : " + prime + "€");
                 }
                 else
                 {
-                    Console.WriteLine(monthCalendar[i] + " : " + salaryNetPerMonth + "€");
+                    Console.WriteLine(monthsCalendar[i] + " : " + salaryNetPerMonth + "€");
                 }
             }
-        }
 
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine("Le pourcentage de la prime doit être un double");
+            Environment.Exit(1);
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine("Le pourcentage de la prime ne peut pas être égal à 0");
+            Environment.Exit(1);
+        }
     }
+
+}
+
